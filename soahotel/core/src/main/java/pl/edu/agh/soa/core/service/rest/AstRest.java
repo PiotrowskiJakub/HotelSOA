1f3d8d8e894aff1a79a609bca987dee070711fbe
Jakub Piotrowski <piotrowski.jakub1993@gmail.com>
2015-06-14 13:19:24 +0200

    Remove camel.

--- soahotel/core/src/main/java/pl/edu/agh/soa/core/service/rest/AstRest.java
+++ soahotel/core/src/main/java/pl/edu/agh/soa/core/service/rest/AstRest.java
@@ -1,11 +1,7 @@
 package pl.edu.agh.soa.core.service.rest;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -13,12 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.apache.camel.ProducerTemplate;
-import org.apache.camel.cdi.CdiCamelContext;
-import org.apache.camel.cdi.ContextName;
-
 import pl.edu.agh.soa.core.bean.AdditionalServiceType;
-import pl.edu.agh.soa.core.dict.EmailMessages;
 import pl.edu.agh.soa.core.service.AstService;
 
 @Stateless
@@ -28,21 +19,10 @@ public class AstRest {
 	@EJB
 	private AstService astService;
 
-	@Inject
-	@ContextName("camel-context")
-	CdiCamelContext context;
-
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public AdditionalServiceType getAst() {
 		AdditionalServiceType ast = astService.getAst();
-
-		// Sample camel proucer
-		ProducerTemplate producer = context.createProducerTemplate();
-		Map<String, Object> headers = new HashMap<String, Object>();
-		headers.put("To", "qballonix@gmail.com");
-		headers.put("Subject", EmailMessages.SUBJECT);
-		producer.sendBodyAndHeaders("direct:mail", EmailMessages.MESSAGE, headers);
 
 		return ast;
 	}
--- soahotel/core/src/main/java/pl/edu/agh/soa/core/service/rest/RegisterWS.java
+++ soahotel/core/src/main/java/pl/edu/agh/soa/core/service/rest/RegisterWS.java
@@ -1,12 +1,9 @@
 package pl.edu.agh.soa.core.service.rest;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -15,12 +12,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.apache.camel.ProducerTemplate;
-import org.apache.camel.cdi.CdiCamelContext;
-import org.apache.camel.cdi.ContextName;
-
 import pl.edu.agh.soa.core.bean.Account;
-import pl.edu.agh.soa.core.dict.EmailMessages;
 import pl.edu.agh.soa.core.service.RegistrationService;
 
 @Stateless
@@ -30,24 +22,12 @@ public class RegisterWS {
 	@EJB
 	RegistrationService registrationService;
 
-	@Inject
-	@ContextName("camel-context")
-	CdiCamelContext context;
-
 	// @Override
 	@POST
 	@Path("account")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response addAccount(Account account) {
 		registrationService.addAccount(account);
-
-		// Send email to new user
-		ProducerTemplate producer = context.createProducerTemplate();
-		Map<String, Object> headers = new HashMap<String, Object>();
-		headers.put("To", account.getContact().getMail());
-		headers.put("Subject", EmailMessages.SUBJECT);
-		producer.sendBodyAndHeaders("direct:mail", account.getFirstName()
-				+ EmailMessages.MESSAGE, headers);
 
 		return Response.ok().build();
 	}
