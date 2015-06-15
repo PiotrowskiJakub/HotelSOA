ab429bf3d4725bcea133796a336430a18869054d
Jakub Piotrowski <piotrowski.jakub1993@gmail.com>
2015-06-16 00:06:36 +0200

    Sample mail after registration using java mail.

--- soahotel/core/src/main/java/pl/edu/agh/soa/core/service/rest/RegisterWS.java
+++ soahotel/core/src/main/java/pl/edu/agh/soa/core/service/rest/RegisterWS.java
@@ -1,9 +1,23 @@
 package pl.edu.agh.soa.core.service.rest;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.List;
+import java.util.Properties;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -29,6 +43,7 @@ public class RegisterWS {
 	public Response addAccount(Account account) {
 		registrationService.addAccount(account);
 
+		sendMial();
 		return Response.ok().build();
 	}
 
@@ -65,4 +80,58 @@ public class RegisterWS {
 		return null;
 	}
 
+	private void sendMial() {
+		configureSslCertificates();
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+
+		Session session = Session.getInstance(props,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(
+								"qballonix@gmail.com", "line6spideriii");
+					}
+				});
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("qballonix@gmail.com"));
+			message.addRecipients(Message.RecipientType.TO,
+					InternetAddress.parse("qballonix@gmail.com"));
+			message.setSubject("SOAHOTEL");
+			message.setText("SOAHOTEL");
+
+			Transport.send(message);
+		} catch (MessagingException e) {
+			// throw new RuntimeException(e);
+		}
+	}
+
+	private void configureSslCertificates() {
+		SSLContext ctx = null;
+		TrustManager[] trustAllCerts = new X509TrustManager[] { new X509TrustManager() {
+			public X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+
+			public void checkClientTrusted(X509Certificate[] certs,
+					String authType) {
+			}
+
+			public void checkServerTrusted(X509Certificate[] certs,
+					String authType) {
+			}
+		} };
+		try {
+			ctx = SSLContext.getInstance("SSL");
+			ctx.init(null, trustAllCerts, null);
+		} catch (NoSuchAlgorithmException | KeyManagementException e) {
+		}
+
+		SSLContext.setDefault(ctx);
+	}
 }
