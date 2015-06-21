cec5d80b0b1c38bcdd059943a74d31dba7663a12
Jakub Piotrowski <piotrowski.jakub1993@gmail.com>
2015-06-21 23:56:44 +0200

    Add service to sends mails.

--- soahotel/core/src/main/java/pl/edu/agh/soa/core/dict/EmailMessages.java
+++ soahotel/core/src/main/java/pl/edu/agh/soa/core/dict/EmailMessages.java
@@ -5,4 +5,9 @@ public interface EmailMessages {
 	public static final String SUBJECT = "[SOAHotel] Twoje konto zostało utworzone";
 	public static final String MESSAGE = ",\n\nTwoje konto w systemie SoaHotel zostało utworzone.\n\nKliknij w poniższy link, aby aktywować konto:\n" + ACCOUNT_ACTIVATION_LINK;
 	public static final String END_MESSAGE = "\n\nPozdrawiamy,\nZespół SoaHotel";
+	
+	public static final String COMPLAITN_SUBJECT = "[SOAHotel] Twoja reklamacja została zgłoszona";
+	public static final String COMPLAINT_MESSAGE = ",\n\nTwoja reklamacja została zgłoszona.\n\n\"";
+	public static final String COMPLAITN_EMPLOYEE_SUBJECT = "Zgłoszono reklamacje";
+	public static final String COMPLAINT_EMPLOYEE_MESSAGE = " zgłosił reklamację o treści:\n\n\"";
 }
